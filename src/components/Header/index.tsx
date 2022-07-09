c8875629190b7ec39c2392ab403cbc52b1f9e7e0
lucassodresa <lucas.sodresa@gmail.com>
2022-07-09 11:45:19 +0100

    Sign In Button Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
+import { SigInButton } from "../SigInButton";
+import Image from "next/image";
 import styles from "./styles.module.scss";
+
 export const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image src="/images/logo.svg" alt="ig.news" width="110" height="31" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+
+        <SigInButton />
       </div>
     </header>
   );
---
+++ src/components/SigInButton/index.tsx
@@ -0,0 +1,19 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
+export const SigInButton = () => {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Lucas Sá
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with GitHub
+    </button>
+  );
+};
